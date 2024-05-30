1fe0eb2f48a0d258654a061b1bdad989688d3a9d
lemontea619 <rain_619@icloud.com>
2024-05-30 16:16:45 +0900

    フッター制作

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -12,39 +12,61 @@ import {
 export function Footer() {
   return (
     <FlowbiteFooter container>
-      <div className="w-full">
+      <div className="w-full ">
         <div className="grid w-full justify-between sm:flex sm:justify-between md:flex md:grid-cols-1">
           <div>
             <FlowbiteFooter.Brand
               href="https://flowbite.com"
-              src="https://flowbite.com/docs/images/logo.svg"
+              src="/vercel.svg"
               alt="Flowbite Logo"
-              name="Flowbite"
+              name="myufes'24"
             />
           </div>
           <div className="grid grid-cols-2 gap-8 sm:mt-4 sm:grid-cols-3 sm:gap-6">
             <div>
-              <FlowbiteFooter.Title title="about" />
+              {/* <FlowbiteFooter.Title title="about" /> */}
               <FlowbiteFooter.LinkGroup col>
-                <FlowbiteFooter.Link href="#">Flowbite</FlowbiteFooter.Link>
-                <FlowbiteFooter.Link href="#">Tailwind CSS</FlowbiteFooter.Link>
-              </FlowbiteFooter.LinkGroup>
-            </div>
-            <div>
-              <FlowbiteFooter.Title title="Follow us" />
-              <FlowbiteFooter.LinkGroup col>
-                <FlowbiteFooter.Link href="#">Github</FlowbiteFooter.Link>
-                <FlowbiteFooter.Link href="#">Discord</FlowbiteFooter.Link>
-              </FlowbiteFooter.LinkGroup>
-            </div>
-            <div>
-              <FlowbiteFooter.Title title="Legal" />
-              <FlowbiteFooter.LinkGroup col>
-                <FlowbiteFooter.Link href="#">
-                  Privacy Policy
+                <FlowbiteFooter.Link href="#" className="text-2xl">
+                  トップページ
                 </FlowbiteFooter.Link>
+                <FlowbiteFooter.Link href="#" className="text-2xl">
+                  お知らせ
+                </FlowbiteFooter.Link>
+                <FlowbiteFooter.Title
+                  title="企画情報"
+                  className="text-2xl"
+                ></FlowbiteFooter.Title>
+                <ul>
+                  <li>
+                    <FlowbiteFooter.Link href="#" className="text-xl">
+                      学内展示
+                    </FlowbiteFooter.Link>
+                  </li>
+                  <li>
+                    <FlowbiteFooter.Link href="#" className="text-xl">
+                      タイムテーブル
+                    </FlowbiteFooter.Link>
+                  </li>
+                  <li>
+                    <FlowbiteFooter.Link href="#" className="text-xl">
+                      ステージ企画
+                    </FlowbiteFooter.Link>
+                  </li>
+                  <li>
+                    <FlowbiteFooter.Link href="#" className="text-xl">
+                      学内マップ
+                    </FlowbiteFooter.Link>
+                  </li>
+                </ul>
+              </FlowbiteFooter.LinkGroup>
+            </div>
+            <div>
+              <FlowbiteFooter.LinkGroup col className="text-2xl">
+                <FlowbiteFooter.Link href="#">露店</FlowbiteFooter.Link>
+                <FlowbiteFooter.Link href="#">アクセス</FlowbiteFooter.Link>
+                <FlowbiteFooter.Link href="#">協賛企業</FlowbiteFooter.Link>
                 <FlowbiteFooter.Link href="#">
-                  Terms &amp; Conditions
+                  ご来場の皆様へ
                 </FlowbiteFooter.Link>
               </FlowbiteFooter.LinkGroup>
             </div>
@@ -52,12 +74,13 @@ export function Footer() {
         </div>
         <FlowbiteFooter.Divider />
         <div className="w-full sm:flex sm:items-center sm:justify-between">
-          <FlowbiteFooter.Copyright href="#" by="Flowbite™" year={2022} />
+          <FlowbiteFooter.Copyright
+            href="#"
+            by="宮城大学大学祭実行委員会"
+            year={2024}
+          />
           <div className="mt-4 flex space-x-6 sm:mt-0 sm:justify-center">
-            <FlowbiteFooter.Icon href="#" icon={BsFacebook} />
             <FlowbiteFooter.Icon href="#" icon={BsInstagram} />
-            <FlowbiteFooter.Icon href="#" icon={BsTwitter} />
-            <FlowbiteFooter.Icon href="#" icon={BsGithub} />
             <FlowbiteFooter.Icon href="#" icon={BsTwitterX} />
           </div>
         </div>
